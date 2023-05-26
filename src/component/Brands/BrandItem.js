856c5c78ffcdc88ffffca0b2f2830cb2004d5455
abanu <alex.banu14@gmail.com>
2023-05-26 20:47:45 +0300

    Don't toggle brand item when clicking buttons

--- src/component/Brands/BrandItem.js
+++ src/component/Brands/BrandItem.js
@@ -23,18 +23,20 @@ const BrandItem = (props) => {
   const brand = props.brand;
 
   return (
-    <div
-      className="brand-item"
-      onClick={() => props.onBrandClick(props.brand.name)}
-    >
-      <h3>{brand.name}</h3>
-      <hr />
-      <p>{brand.description || "No description provided"}</p>
+    <div className="brand-item">
+      <div onClick={() => props.onBrandClick(brand.name)}>
+        <h3>{brand.name}</h3>
+        <hr />
+        <p>{brand.description || "No description provided"}</p>
+      </div>
       {/* Show Edit/Delete buttons only if this card is toggled. */}
       {props.toggledBrand === brand.name && (
         <div>
-          <button>Edit</button>
-          <button onClick={() => props.onDeleteBrand(brand.name)}>
+          <button type="button">Edit</button>
+          <button
+            type="buttotn"
+            onClick={() => props.onDeleteBrand(brand.name)}
+          >
             Delete
           </button>
         </div>
